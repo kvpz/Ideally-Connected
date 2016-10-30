43bd788e130874120c9923522c8e7c48cbe130a4
Kevin Perez <unknotted26@gmail.com>
2016-10-30 14:56:31 -0400

    Reverse engineered the database asnet-IdeallyConnectedWebApp-370.. to IdeallyConnectedWebApp

---
+++ IdeallyConnectedWebApp/Controllers/BlogsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdeallyConnectedWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdeallyConnectedWebApp.Controllers
+{
+    public class BlogsController : Controller
+    {
+        private BloggingContext _context;
+
+        public BlogsController(BloggingContext context)
+        /*
+            ASP.NET dependency injection will take care of passing an instance of BloggingContext into controller.
+        */
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Display all blogs in the database.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View(_context.Blogs.ToList());
+        }
+
+        /// <summary>
+        /// Insert new blogs in the database.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Blogs.Add(blog);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(blog);
+        }
+    }
+}
--- IdeallyConnectedWebApp/README.md
+++ IdeallyConnectedWebApp/README.md
@@ -3,3 +3,6 @@
 **To add new database**
 Add-Migration -c BloggingContext
 Add-Database -c BloggingContext
+
+**To add/ reverse engineer existing database**
+Scaffold-DbContext "Server=(localdb)\mssqllocaldb;Database=Blogging;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models
