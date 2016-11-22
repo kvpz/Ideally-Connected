740a79e441b1fedcddd09b199db02be29653ace5
Kevin Perez <unknotted26@gmail.com>
2016-11-22 18:13:37 -0500

    Custom application models will be in here.

---
+++ IdeallyConnectedWebApi_pureNetFramework/Models/AppModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace IdeallyConnectedWebApi_pureNetFramework.Models
+{
+    public class UserProfile : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Biography { get; set; }
+        public int Age { get; set; }
+    }
+}
