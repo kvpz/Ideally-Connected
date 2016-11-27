c47995e35057d0bf5c6d3e120c4ed02ccf552b7b
Kevin Perez <unknotted26@gmail.com>
2016-11-27 15:06:31 -0500

    New Migration-Table UserProfile

--- IdeallyConnectedWebApi_pureNetFramework/Models/AppModel.cs
+++ IdeallyConnectedWebApi_pureNetFramework/Models/AppModel.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace IdeallyConnectedWebApi_pureNetFramework.Models
 {
-    public class UserProfile : IdentityUser
+    public class UserProfile //: IdentityUser
     {
+        [Key, ForeignKey("ApplicationUser")]
+        public string Id { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Biography { get; set; }
