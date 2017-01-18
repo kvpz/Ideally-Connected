6747813fa59b1febb207e658c38e9be0e0b38d46
Kevin Perez <unknotted26@gmail.com>
2017-01-18 18:50:26 -0500

    Software entity will now have an Enum of Software Types

--- IdeallyConnected/Models/Software.cs
+++ IdeallyConnected/Models/Software.cs
@@ -6,9 +6,22 @@ using System.Web;
 
 namespace IdeallyConnected.Models
 {
+    public enum SoftwareTypes 
+    {
+        OperatingSystem,
+        TextEditor,
+        Presentation,
+        PhotoEditor,
+        Gaming,
+        Design,
+        Browser,
+        ProgrammingTool
+    }
+
     public class Software
     {
         [Key]
         public String Id { get; set; }
+        public SoftwareTypes Type { get; set; }
     }
 }
