582acd476d7eb3d3565391bbe32f272020cf5e18
Kevin Perez <unknotted26@gmail.com>
2017-01-17 22:22:37 -0500

    Changes to the model before more changes..

--- IdeallyConnected/Models/User.cs
+++ IdeallyConnected/Models/User.cs
@@ -6,17 +6,14 @@ using System.Web;
 
 namespace IdeallyConnected.Models
 {
-    public class User
+    public class User : ApplicationUser
     {
         public int UserID { get; set; }
         
         [Required()]
         [StringLength(100, MinimumLength = 2)]
         public string Name { get; set; }
-
-        public virtual List<Skill> Skills { get; set; }
         public virtual UserProfile UserProfile { get; set; }
-        //[ConcurrencyCheck()]
         
     }
 }
--- IdeallyConnected/Models/UserProfile.cs
+++ IdeallyConnected/Models/UserProfile.cs
@@ -7,13 +7,20 @@ using System.Web;
 
 namespace IdeallyConnected.Models
 {
+    /*
+        1-1 relation with User; foreign key does not need to be specified.
+        A unidirectional relationship is established by only defining the navigation property
+        on only one of the types that participates in the relationship and not both.
+    */
     public class UserProfile 
     {
-        [Key, ForeignKey("ApplicationUser")]
+        [Key, ForeignKey("User")] //("ApplicationUser")]
         public string UserId { get; set; }
         public string Biography { get; set; }
         public virtual User User { get; set; }
-        
+
+        public virtual List<ProgrammingLanguages> ProgrammingLanguages { get; set; }
+        public virtual List<Software> Software { get; set; }
         /*
             Github account
             StackOverflow account
