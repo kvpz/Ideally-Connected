6c9ca369fa4baae05dfb20b7860dae9b5d9272bd
Kevin Perez <unknotted26@gmail.com>
2017-01-18 17:13:43 -0500

    other model changes

---
+++ IdeallyConnected/Models/ProgrammingLanguages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IdeallyConnected.Models
+{    
+    /*
+        There are not many Programming Languages so I think a string id is most appropriate.
+    */
+    public class ProgrammingLanguages
+    {
+        [Key]
+        public String Id { get; set; }
+
+    }
+}
