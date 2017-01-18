e7774279366aa8a090dabbf3dd33141a842daf78
Kevin Perez <unknotted26@gmail.com>
2017-01-18 17:13:43 -0500

    other model changes

---
+++ IdeallyConnected/Models/Software.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IdeallyConnected.Models
+{
+    public class Software
+    {
+        [Key]
+        public String Id { get; set; }
+    }
+}
