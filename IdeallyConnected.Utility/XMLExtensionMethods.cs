166e2b081aa585023a0a3a9e96fd30c934fbd0b5
Kevin Perez <unknotted26@gmail.com>
2017-02-17 16:02:00 -0500

    XML utility methodadded to IdeallyConnected.Utility

---
+++ IdeallyConnected.Utility/XMLExtensionMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace IdeallyConnected.Utility.Components
+{
+    public static class XMLExtensionMethods
+    {
+        public static T GetAs<T>(this XElement element, T defaultValue = default(T))
+        {
+            T value = defaultValue;
+            
+            if(element != null && !string.IsNullOrEmpty(element.Value))
+            {
+                value = (T)Convert.ChangeType(element.Value, typeof(T));
+            }
+
+            return value;
+        }
+    }
+}
