fbe1f1a5d703e9fadfb120c7ed7e518a7f97da4b
Kevin Perez <unknotted26@gmail.com>
2017-04-26 00:29:33 -0400

    Created Utility class.

---
+++ IdeallyConnected.Database/Utility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeallyConnected.TestDatabases
+{
+    class Utility
+    {
+    }
+
+    public static class MiscUtility
+    {
+        public static void WriteLineFormatted(string str, ConsoleColor foregroundColor)
+        {
+            Console.ForegroundColor = foregroundColor;
+            Console.WriteLine(str);
+            Console.ResetColor();
+        }
+    }
+}
