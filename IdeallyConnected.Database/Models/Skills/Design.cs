b2087f00aa013d14b895b87686e89c84ce362ece
Kevin Perez <unknotted26@gmail.com>
2017-04-26 20:06:40 -0400

    Added IdeallyConnected database models to database project.

---
+++ IdeallyConnected.Database/Models/Skills/Design.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace IdeallyConnected.Models
+{
+    [Table("DesignSkills")]
+    public class Design : Skill
+    {
+        public Design() : base(SkillEnum.Design, 0x00) {}
+
+        public string TypeOfDesign { get; set; }
+        public string Software { get; set; }
+    }
+}
