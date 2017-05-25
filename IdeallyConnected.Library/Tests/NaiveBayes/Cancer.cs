496cbc8294a5b6ab07f1dd9b6e933634a19662d4
Kevin Perez <unknotted26@gmail.com>
2017-05-24 23:20:12 -0400

    Added built model verification tests.

---
+++ IdeallyConnected.Library/Tests/NaiveBayes/Cancer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace IdeallyConnected.Library.Tests.NaiveBayes
+{
+    public class Cancer
+    {
+        public Dictionary<string, string> Attribute { get; set; }
+    }
+
+    /*
+        Stores attributes as hard-coded properties, or efficiently via
+        the inherited Cancer class.
+    */
+    public class BreastCancer : Cancer, IModel
+    {
+        public BreastCancer() { }
+
+        public BreastCancer(string data)
+        {
+            this.Attribute = new Dictionary<string, string>();
+            string[] attributes = data.Split(' ');
+            ClassLabel = attributes[0];
+            for(int i = 1; i < attributes.Length; ++i)
+            {
+                string[] attributeAndData = attributes[i].Split(':');
+                this[attributeAndData[0]] = attributeAndData[1];
+                this.Attribute[attributeAndData[0]] = attributeAndData[1];
+            }
+        }
+
+        public object this[string attribute]
+        {
+            get
+            {
+                PropertyInfo attr = typeof(BreastCancer).GetRuntimeProperty("A" + attribute);
+                return attr.GetValue(this, null);
+            }
+            set
+            {
+                PropertyInfo attr = typeof(BreastCancer).GetRuntimeProperty("A" + attribute);
+                attr.SetValue(this, value);
+            }
+        }
+
+        public string ClassLabel { get; set; }
+
+        #region class attributes
+        public string A1 { get; set; }
+        public string A2 { get; set; }
+        public string A3 { get; set; }
+        public string A4 { get; set; }
+        public string A5 { get; set; }
+        public string A6 { get; set; }
+        public string A7 { get; set; }
+        public string A8 { get; set; }
+        public string A9 { get; set; }
+        #endregion
+
+        public object Create(string datafile)
+        {
+            return new BreastCancer(datafile);
+        }
+    }
+}
---
+++ IdeallyConnected.Library/Tests/NaiveBayes/NaiveBayesTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace IdeallyConnected.Library.Tests.NaiveBayes
+{
+    
+    public class NaiveBayesTests
+    {
+        [Fact]
+        public void ModelDataEquivalentToFileData()
+        {
+            string datafile = "..\\..\\..\\Tests\\NaiveBayes\\breast_cancer.train";
+            Model<BreastCancer> model = new Model<BreastCancer>();
+            model.PopulateModel(datafile);
+            // Check that model was created
+            Assert.False(model == null);
+
+            // Check that data was loaded
+            Assert.False(model.Records.Count == 0);
+
+            // Check that model data matches file data
+            int record_i = 0;
+            foreach(string frecord in File.ReadAllLines(datafile))
+            {
+                BreastCancer mrecord = model.Records[record_i++];
+                string[] attributes = frecord.Split(' ');
+                Assert.Equal(mrecord.ClassLabel, attributes[0]);
+                for(int a_i = 1; a_i < attributes.Length; ++a_i)
+                {
+                    string attributeName = attributes[a_i].Split(':')[0];
+                    string data = attributes[a_i].Split(':')[1];
+                    // Check if attribute was stored
+                    Assert.True(mrecord.Attribute.ContainsKey(attributeName), $"Attribute {attributeName} is not stored in model.");
+                    // Check if attribute's data is correct
+                    Assert.Equal(data, mrecord.Attribute[attributeName]);
+                }
+            }
+        }
+
+    }
+}
